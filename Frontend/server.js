36292e9173b2382eb9167b4e593459de0714cb41
sergio9508 <menjivarsergio@gmail.com>
2018-04-30 21:17:27 -0600

    cambio de path

--- Frontend/server.js
+++ Frontend/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('./'))
+app.use(express.static('./MantenimientoTPI-web'))
 
 app.listen(3000, ()=>
     console.log('servidor iniciado')
