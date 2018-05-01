7f8a55bb1f83a80551f015d2fd660a040872e1f5
Ronald Ibanez <ronald_reir21@hotmail.com>
2018-05-01 00:33:34 -0600

    funcional, se carga con la url: localhost:3000, se esta usando los rest de la API

--- Frontend/wc-DinamicTb.js
+++ Frontend/wc-DinamicTb.js
@@ -7,7 +7,7 @@ class dinamicTable extends HTMLElement {
     connectedCallback(){
         let shadow = this.attachShadow({mode: 'open'}); //raiz del shadowDom
 
-        //let prom = this.path(`http://localhost:8080/MantenimientoTPI-web/webresources/${this.getAttribute('from')}`);
+       // let prom = this.path(`http://localhost:8080/MantenimientoTPI-web/webresources/${this.getAttribute('from')}`);
       //  let prom = this.path(`https://jsonplaceholder.typicode.com/${this.getAttribute('from')}`);
 
         let prom = this.path(`http://172.25.0.29:8181/MantenimientoTPI-web/webresources/${this.getAttribute('from')}`);
